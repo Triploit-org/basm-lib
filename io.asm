ba568d7e53ef15eda6a218f595ba164e09cc0113
Survari <tristanpieper080803@gmail.com>
2017-03-02 19:16:36 +0100

    Update io.asm

--- io.asm
+++ io.asm
@@ -5,14 +5,35 @@ write_string: # needs string value on Stack, Length on String in HX
 		pop
 	ggn beg
 	return
+	
+write_int: # needs string value on Stack, Length on String in HX
+	.beg:
+		sub 1, hx
+		outl
+		pop
+	ggn beg
+	return
 
-write_array: # needs string value in LIST_AX
+write_array_char: # needs string value in LIST_AX
 	ags list_ax, hx
 	mov 0, ax
 	.beg:
 		aga list_ax, ax, bx
 		push bx
 		ptc
+		pop
+		add 1, ax
+		sub 1, hx
+	ggn beg
+	return
+	
+write_array_int: # needs string value in LIST_AX
+	ags list_ax, hx
+	mov 0, ax
+	.beg:
+		aga list_ax, ax, bx
+		push bx
+		outl
 		pop
 		add 1, ax
 		sub 1, hx
