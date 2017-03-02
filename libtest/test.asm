02f045f14e5821e6e387d8d86396d0a3ea210eeb
triploit <tristanpieper080803@gmail.com>
2017-03-02 16:07:36 +0100

    Updated

--- libtest/test.asm
+++ libtest/test.asm
@@ -5,11 +5,14 @@ main:
     var o_in
     inp
     mvv hx, o_in
+
     jmp stack_to_array
     jmp array_to_stack
+
     pushv o_in
     str hx
     pop
+
     jmp write_string
     jmp new_line
     hlt
--- values.asm
+++ values.asm
@@ -1,8 +1,6 @@
-stack_to_array: # needs length of input in hx, output in list_ax; if yout get input with inp,
-				# the value is on the stack and the length of the input is in hx,
-				# so be careful with hx!
-	var len
-	mvv hx, len
+stack_to_array:			# needs length of input in hx, output in list_ax; if yout get input with inp,
+	var len 			# the value is on the stack and the length of the input is in hx,
+	mvv hx, len			# so be careful with hx!
 	.ar:
 		ata list_ax, 0
 		sub 1, hx
@@ -19,11 +17,9 @@ stack_to_array: # needs length of input in hx, output in list_ax; if yout get in
 	ggn beg
 	return
 
-array_to_stack: # needs value in list_ax
-				# "asa your_array, list_ax"
-				# -> This sets the value of list_ax to your_array
-	ags list_ax, hx
-	mov 0, fx
+array_to_stack:			# needs value in list_ax
+	ags list_ax, hx		# "asa your_array, list_ax"
+	mov 0, fx			# -> This sets the value of list_ax to your_array
 	.beg:
 		aga list_ax, fx, gx
 		push gx
